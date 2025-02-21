f61511abd62c1277ba92efca90c560f0098a471f
hjames30 <hjames30@student.cccs.edu>
2025-02-21 13:06:58 -0700

    2/21

---
+++ LeetCodePractice/src/RomanNumber.java
@@ -0,0 +1,21 @@
+
+public class RomanNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String romanToInt(String s) {
+		for(int i =0; i<s.length();i++) {
+			s.replace('I', '1');
+			s.replace('V', '5');
+			
+			
+			
+		}
+		return s;
+		
+		
+	}
+
+}
