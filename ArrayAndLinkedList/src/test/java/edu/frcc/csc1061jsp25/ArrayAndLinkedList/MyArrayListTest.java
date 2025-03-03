e114c6f2a199f5e32486f8b0f25990f7a24574f7
hjames30 <hjames30@student.cccs.edu>
2025-03-03 14:35:15 -0700

    3/3

---
+++ ArrayAndLinkedList/src/test/java/edu/frcc/csc1061jsp25/ArrayAndLinkedList/MyArrayListTest.java
@@ -0,0 +1,31 @@
+package edu.frcc.csc1061jsp25.ArrayAndLinkedList;
+
+import java.util.List;
+
+public class MyArrayListTest {
+
+	public static void main(String[] args) {
+		List<Integer> ml = new MyArrayList<Integer>();
+		
+		
+		ml.add(1);
+		ml.add(2);
+		ml.add(3);
+		ml.add(4);
+		ml.add(5);
+		ml.add(6);
+		ml.add(7);
+		ml.add(8);
+		for(int i = 0;i<ml.size();i++) {
+			System.out.print(ml.get(i)+" ");
+		}
+		System.out.println();
+		
+		ml.remove(ml.indexOf(3));
+		for(int i = 0;i<ml.size();i++) {
+			System.out.print(ml.get(i)+" ");
+		}
+
+	}
+
+}
