4f5f0a94a605d895f67bb865a4987fdc31bb5343
hjames30 <hjames30@student.cccs.edu>
2025-05-02 14:40:18 -0600

    5/2

--- MinHeap/src/test/java/edu/frcc/csc1061jsp25/MinHeap/MinHeapTest.java
+++ MinHeap/src/test/java/edu/frcc/csc1061jsp25/MinHeap/MinHeapTest.java
@@ -1,5 +1,8 @@
 package edu.frcc.csc1061jsp25.MinHeap;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class MinHeapTest {
 
 	public static void main(String[] args) {
@@ -14,7 +17,26 @@ public class MinHeapTest {
 			System.out.print(minHeap.remove()+" ");
 		}
 		System.out.println();
-
+		
+		PriorityQueue<Integer> qheap = new PriorityQueue<>();
+		for(int node : nodes) {
+			qheap.add(node);
+		}
+		System.out.println("PQHeap Removal:");
+		while(!qheap.isEmpty()) {
+			System.out.print(qheap.remove()+" ");
+		}
+		System.out.println();
+		//Collections.reverseOrder changes the way it is compared, changing the order it's in 
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		for(int node : nodes) {
+			maxHeap.add(node);
+		}
+		System.out.println("MPQHeap Removal:");
+		while(!maxHeap.isEmpty()) {
+			System.out.print(maxHeap.remove()+" ");
+		}
+		System.out.println();
 	}
 
 }
